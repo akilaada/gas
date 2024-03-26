5d953f3618a85930fbe5b9215db5275171eb7d6c
ada <164998926+akilaada@users.noreply.github.com>
2024-03-26 14:38:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4a39b3ea-ad9a-4173-8a7c-8328abf236d0/)
 
 
 echo "$curl_output" >> Update.tsx
