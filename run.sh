a2767347609a466e2ce44e72dc8904b1b7e5888e
ada <164998926+akilaada@users.noreply.github.com>
2024-05-28 15:02:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4a39b3ea-ad9a-4173-8a7c-8328abf236d0/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-2c1aae42-e35d-42db-8f4c-5e11255b9e95)
 
 
 echo "$curl_output" >> Update.tsx
